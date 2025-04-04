e8301de278a51dfbd7c29430a054c201f85eccaa
Adil Ahmed <adilahmed749@gmail.com>
2025-04-04 10:57:54 +0100

    Selecting elements from the DOM.

---
+++ script.js
@@ -0,0 +1,12 @@
+// Step 1: Select elements from the DOM
+const moodSelect = document.getElementById("mood");
+const colourBtn = document.getElementById("changeColour");
+const colourDisp = document.getElementById("colourCode");
+
+// Step 2: Handling button click events
+
+// Step 3: Function to get a mood-based colour
+
+// Step 4: Convert RGB to HEX
+
+// Step 5: Adding smooth transitions
